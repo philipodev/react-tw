a5c02034c27b12fdc393577a9fd123fde4dabf7e
Philip Andersson <philipodev@gmail.com>
2023-04-21 23:13:51 +0200

    added missing types

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: 'src/index.ts',
